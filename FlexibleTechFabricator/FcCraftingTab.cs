0c9e38e65f5d034944e8ca2a6db403d49a8bbc7a
PrimeSonic <prime.sonic@gmail.com>
2020-01-01 17:24:16 -0500

    FlexibleTechFabricator - A new project that provides a skeleton framework for a fabricator that is built up based on requests from external mods

---
+++ FlexibleTechFabricator/FcCraftingTab.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace FlexibleTechFabricator
+{
+    public interface IFcCraftingTab
+    {
+        string Id { get; }
+        Atlas.Sprite Icon { get; }
+        string DisplayName { get; }
+        void LoadAssets(IFcAssetBundlesService assetBundlesService);
+    }
+
+    public abstract class FcCraftingTab : IFcCraftingTab
+    {
+        public string Id { get; }
+
+        public Atlas.Sprite Icon { get; private set; }
+
+        public string DisplayName { get; }
+
+        public abstract string AssetBundleName { get; }
+
+        public AssetBundle AssetBundle { get; private set; }
+
+        protected FcCraftingTab(string id, string displayName)
+        {
+            this.Id = id;
+            this.DisplayName = displayName;
+        }
+
+        public void LoadAssets(IFcAssetBundlesService assetBundlesService)
+        {
+            if (this.AssetBundle == null)
+            {
+                this.AssetBundle = assetBundlesService.GetAssetBundleByName(this.AssetBundleName);
+            }
+        }
+    }
+}
